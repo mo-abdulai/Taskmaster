5b7397aa9974533308e068ee4ca72572ee008c49
Mohammed Abdulai <nurud43@gmail.com>
2023-03-27 01:09:22 -0400

    cleaning

--- apis/twilio.js
+++ apis/twilio.js
@@ -3,9 +3,6 @@ require('dotenv').config();
 
 module.exports.sendSMS = function(phone, content) {
 
-  // message = "Hello world";
-  // phone = "+17577526127"
-
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
